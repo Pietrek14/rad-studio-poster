6bf0e2dd796b956a6f4e407ed264853bdebed766
Pietrek14 <dawidekpe@gmail.com>
2023-02-08 23:05:33 +0100

    added the data

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
 import logo from "./assets/rad-studio-logo.png";
+import classes from "./assets/data.json";
 import { Class } from "./components/Class";
 
 function App() {
-	const classes = [
-		{ class: "TObject", events: [] },
-		{ class: "TPersistent", events: [] },
-		{
-			class: "TComponent",
-			events: [
-				{ name: "OnGetDeltaStreams", accessModifier: "protected" },
-			],
-		},
-	];
-
 	return (
 		<div className="flex flex-col">
 			<header className="flex justify-center items-center gap-32 py-12">
 				<img src={logo} alt="RAD Studio Logo" className="w-1/5" />
 				<h1 className="text-7xl font-bold">RAD Studio -- Wydarzenia</h1>
 			</header>
-			<main className="flex flex-col items-center gap-6">
+			<main className="flex flex-col items-center gap-6 my-5">
 				{classes.map((classProps, index) => {
 					return (
 						<Class
--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,7 +1,7 @@
 export const Event = ({ event }) => {
 	return (
 		<>
-			<span className="text-xl text-center">{event.name}</span>
+			<span className="text-xl text-center mb-5">{event.name}</span>
 			{event.description ? (
 				<span className="text-xl text-justify text-slate-400">
 					{event.description}
